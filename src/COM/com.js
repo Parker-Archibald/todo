bfc69c94f358635efab34011ff801427c58c8762
ParkerArchibald <parkerarchibald97@gmail.com>
2022-08-28 12:48:25 -0600

    change port

--- src/COM/com.js
+++ src/COM/com.js
@@ -1,2 +1,2 @@
-export const TODO_API = process.env.REACT_APP_TODO_API || "http://localhost:3001/";
-// export const TODO_API = "https://parker-archibald-todo-api.herokuapp.com/";
+// export const TODO_API = process.env.REACT_APP_TODO_API || "http://localhost:3001/";
+export const TODO_API = "https://parker-archibald-todo-api.herokuapp.com/";
