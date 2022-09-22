795328d702ecc4223c8c6e0a90f488d583f3ba6d
Parker <parkerarchibald97@gmail.com>
2022-09-21 22:59:54 -0600

    up to date

--- src/Components/UpdateTodo.js
+++ src/Components/UpdateTodo.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
 import '../Styles/UpdateTodo.css';
+import {TODO_API} from '../COM/com';
 
 const UpdateTodo = (props) => {
 
@@ -8,7 +9,7 @@ const UpdateTodo = (props) => {
     const {from} = location.state;
 
     const [todo, setTodo] = useState({
-        todoId: props.info._id,
+        todoId: props.info.todoId,
         todo_name: props.info.todo_name,
         time: props.info.time,
         notes: props.info.notes,
@@ -23,22 +24,40 @@ const UpdateTodo = (props) => {
         document.getElementById(`${todo.todo_name}updateContainer`).className = 'updateContainer';
     }
 
+    const handleChange = (e) => {
+        setTodo(previousData => {
+            return {...previousData, [e.target.name]: e.target.value}
+        })
+    }
+
+    async function handleConfirm() {
+        await fetch(`${TODO_API}updateTodo/${todo.todoId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(todo)
+        })
+        .then(alert("Todo Updated"))
+        .then(setTimeout("document.location.reload(true)", 200))
+    }
+
     return(
         <div id={`${todo.todo_name}updateContainer`} className='updateContainer'>
             <div id='updateTodoTitle'>
                 Update {todo.todo_name}
             </div>
             <div id='updateTodoName'>
-                Name: <input id='todoNameInput' placeholder={todo.todo_name}/>
+                Name: <input id='todoNameInput' name='todo_name' placeholder={todo.todo_name} onChange={handleChange}/>
             </div>
             <div id='updateTodoTime'>
-                Due by: <input id='updateDueBy' placeholder={todo.time}/>
+                Due by: <input id='updateDueBy' name='time' placeholder={todo.time} onChange={handleChange}/>
             </div>
             <div id='updateTodoNotes'>
-                Notes: <div id='updateNotes'>{todo.notes}</div>
+                Notes: <input id='updateNotes' name='notes' placeholder={todo.notes} onChange={handleChange}/>
             </div>
             <div id='updateButtons'>
-                <div id='updateConfirm'>Confirm</div>
+                <div id='updateConfirm' onClick={handleConfirm}>Confirm</div>
                 <div id='updateCancel' onClick={closeUpdate}>Cancel</div>
             </div>
         </div>
